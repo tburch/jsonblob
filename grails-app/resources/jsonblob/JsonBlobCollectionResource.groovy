09a50e7d975fd785fdc6e73b67792ef63f47db2a
Tristan Burch <me@tristanburch.com>
2012-12-04 19:49:04 -0700

    cleanup imports

--- grails-app/resources/jsonblob/JsonBlobCollectionResource.groovy
+++ grails-app/resources/jsonblob/JsonBlobCollectionResource.groovy
@@ -11,15 +11,9 @@ import com.fasterxml.jackson.databind.module.SimpleModule
 import org.bson.types.ObjectId
 import org.springframework.beans.factory.InitializingBean
 
-import javax.ws.rs.core.UriBuilder
-
-import javax.ws.rs.Consumes
-import javax.ws.rs.GET
-import javax.ws.rs.Produces
-import javax.ws.rs.Path
-import javax.ws.rs.PathParam
-import javax.ws.rs.POST
+import javax.ws.rs.*
 import javax.ws.rs.core.Response
+import javax.ws.rs.core.UriBuilder
 
 @Path('/api/jsonBlob')
 @Consumes(['application/json'])
--- grails-app/resources/jsonblob/JsonBlobResource.groovy
+++ grails-app/resources/jsonblob/JsonBlobResource.groovy
@@ -1,15 +1,7 @@
 package jsonblob
 
-import static org.grails.jaxrs.response.Responses.*
-
-import javax.ws.rs.Consumes
-import javax.ws.rs.DELETE
-import javax.ws.rs.GET
-import javax.ws.rs.Produces
-import javax.ws.rs.PUT
+import javax.ws.rs.*
 import javax.ws.rs.core.Response
-
-import org.grails.jaxrs.provider.DomainObjectNotFoundException
 
 @Consumes(['application/json'])
 @Produces(['application/json'])
