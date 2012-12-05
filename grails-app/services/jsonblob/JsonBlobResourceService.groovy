2f39557f699f5b058941db6523828d6c855856f3
Tristan Burch <me@tristanburch.com>
2012-12-05 11:43:26 -0700

    try to load mongo connection at runtime

--- grails-app/services/jsonblob/JsonBlobResourceService.groovy
+++ grails-app/services/jsonblob/JsonBlobResourceService.groovy
@@ -5,14 +5,13 @@ import com.mongodb.BasicDBObject
 import com.mongodb.util.JSON
 import org.bson.types.ObjectId
 import org.grails.jaxrs.provider.DomainObjectNotFoundException
+import org.springframework.beans.factory.InitializingBean
 
-class JsonBlobResourceService {
+class JsonBlobResourceService implements InitializingBean {
 
     GMongo mongo
 
-    private def blobCollection() {
-        mongo.getDB(mongo.getDatabaseNames().first()).getCollection("blob")
-    }
+    def blobCollection
 
     private def getDBObject(String objectId) {
         new BasicDBObject("_id", new ObjectId(objectId))
@@ -20,12 +19,12 @@ class JsonBlobResourceService {
 
     def create(String json) {
         def parsed = JSON.parse(json)
-        blobCollection().insert(parsed)
+        blobCollection.insert(parsed)
         parsed
     }
 
     def read(String id) {
-        def obj = blobCollection().findOne(getDBObject(id))
+        def obj = blobCollection.findOne(getDBObject(id))
         if (!obj) {
             throw new DomainObjectNotFoundException(ObjectId.class, id)
         }
@@ -33,21 +32,42 @@ class JsonBlobResourceService {
     }
     
     def update(String id, String json) {
-        def obj = blobCollection().findOne(getDBObject(id))
+        def obj = blobCollection.findOne(getDBObject(id))
         if (!obj) {
             throw new DomainObjectNotFoundException(ObjectId.class, id)
         }
         def parsed = com.mongodb.util.JSON.parse(json)
-        blobCollection().update(getDBObject(id), parsed)
+        blobCollection.update(getDBObject(id), parsed)
         parsed
     }
     
     void delete(String id) {
-        def obj = blobCollection().findOne(getDBObject(id))
+        def obj = blobCollection.findOne(getDBObject(id))
         if (obj) {
-            blobCollection().remove(getDBObject(id))
+            blobCollection.remove(getDBObject(id))
         }
     }
 
+    void afterPropertiesSet() throws Exception {
+        def db
+        if (System.env.MONGOHQ_URL) {
+            def uri = new URI(System.env.MONGOHQ_URL)
+            def host = uri.host
+            def port = uri.port
+            def username = uri.userInfo.split(":")[0]
+            def password = uri.userInfo.split(":")[1]
+            def databaseName = uri.path.substring(1)
+
+            log.info("Setting up mongo using config $System.env.MONGOHQ_URL. Parsed into host=$host, port=$port, username=$username, databaseName=$databaseName")
+
+            def mongo = new GMongo(host, port)
+
+            db = mongo.getDB(mongoDatabaseName)
+            db.authenticate(username, password.toCharArray())
+        } else {
+            db = mongo.getDB(mongo.getDatabaseNames().first())
+        }
+        this.blobCollection = db.getCollection("blob")
+    }
 }
 
