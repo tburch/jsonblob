db91ec010dbbf387620b529eb7bdde9ac482341a
Tristan Burch <me@tristanburch.com>
2012-12-06 22:20:02 -0700

    add health check controller

---
+++ grails-app/controllers/jsonblob/HealthCheckController.groovy
@@ -0,0 +1,12 @@
+package jsonblob
+
+import grails.converters.JSON
+
+class HealthCheckController {
+    def status() {
+        render(contentType: 'text/json') {
+            [status: "OK",
+            timestamp: new Date()]
+        }
+    }
+}
