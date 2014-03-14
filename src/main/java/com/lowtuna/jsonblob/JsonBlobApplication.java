5a0e155432ffdbfee47bc83878d7c734ccdb69dc
Tristan Burch <me@tristanburch.com>
2014-03-14 11:19:18 -0600

    add gittip header

--- src/main/java/com/lowtuna/jsonblob/JsonBlobApplication.java
+++ src/main/java/com/lowtuna/jsonblob/JsonBlobApplication.java
@@ -10,6 +10,7 @@ import com.lowtuna.jsonblob.core.BlobManager;
 import com.lowtuna.jsonblob.healthcheck.MongoHealthCheck;
 import com.lowtuna.jsonblob.resource.ApiResource;
 import com.lowtuna.jsonblob.resource.JsonBlobEditorResource;
+import com.lowtuna.jsonblob.util.jersey.GitTipHeaderFilter;
 import com.lowtuna.jsonblob.util.mongo.JacksonMongoDbModule;
 import com.mongodb.DB;
 import io.dropwizard.Application;
@@ -88,6 +89,7 @@ public class JsonBlobApplication extends Application<JsonBlobConfiguration> {
         environment.jersey().register(new ApiResource(blobManager, configuration.getBlobManagerConfig().isDeleteEnabled(), configuration.getGoogleAnalyticsConfig()));
         environment.jersey().register(new JsonBlobEditorResource(blobManager, configuration.getGoogleAnalyticsConfig()));
         environment.jersey().register(RequestIdFilter.class);
+        environment.jersey().register(GitTipHeaderFilter.class);
     }
 
 }
