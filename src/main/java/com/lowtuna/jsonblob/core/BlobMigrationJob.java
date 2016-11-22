bd5ff916974b198498f46260ebc8706bc7c28ae0
Tristan Burch <me@tristanburch.com>
2016-11-21 18:11:45 -0700

    skip 50

--- src/main/java/com/lowtuna/jsonblob/core/BlobMigrationJob.java
+++ src/main/java/com/lowtuna/jsonblob/core/BlobMigrationJob.java
@@ -1,12 +1,17 @@
 package com.lowtuna.jsonblob.core;
 
 import com.google.common.base.Stopwatch;
+import com.mongodb.BasicDBObject;
+import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoException;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
+import java.util.Date;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -21,7 +26,7 @@ public class BlobMigrationJob implements Runnable {
 
   private final MongoDbJsonBlobManager mongoDbJsonBlobManager;
   private final FileSystemJsonBlobManager fileSystemJsonBlobManager;
-  private final ExecutorService executorService = Executors.newCachedThreadPool();
+  private final ExecutorService executorService = Executors.newFixedThreadPool(50);
 
   public BlobMigrationJob(MongoDbJsonBlobManager mongoDbJsonBlobManager, FileSystemJsonBlobManager fileSystemJsonBlobManager) {
     this.mongoDbJsonBlobManager = mongoDbJsonBlobManager;
@@ -37,41 +42,42 @@ public class BlobMigrationJob implements Runnable {
     log.info("Starting blob migration");
     int limit = 2500;
     final CountDownLatch latch = new CountDownLatch(limit);
-
-    DBCursor curs = mongoDbJsonBlobManager.getCollection().find().limit(limit);
+    DBCursor curs = mongoDbJsonBlobManager.getCollection().find().skip(50).limit(limit);
     try {
       while (curs.hasNext()) {
           if (curs.hasNext()) {
             DBObject o = curs.next();
-
-            executorService.submit(new Runnable() {
-              @Override
-              public void run() {
-                int completed = migratedBlobs.incrementAndGet();
-                ObjectId id = (ObjectId) o.get(MongoDbJsonBlobManager.ID_ATTR_NAME);
-                try {
-                  String json = o.get(MongoDbJsonBlobManager.BLOB_ATTR_NAME).toString();
-
-                  log.trace("Migrating blob {}", id);
-                  fileSystemJsonBlobManager.createBlob(json, id.toString());
-                  log.trace("Completed migrating blob {}", id);
-                } catch (MongoException e) {
-                  log.warn("Error while migrating blob", e);
-                } catch (Exception e) {
-                  log.warn("Caught exception while migrating blob", e);
-                } finally {
-                  latch.countDown();
-                  if (completed % 25 == 0) {
-                    log.info("Migrated {} blobs... (~{} per second)", completed, completed / stopwatch.elapsed(TimeUnit.SECONDS));
-                  }
+            ObjectId id = (ObjectId) o.get(MongoDbJsonBlobManager.ID_ATTR_NAME);
+            try {
+              final String json = o.get(MongoDbJsonBlobManager.BLOB_ATTR_NAME).toString();
+              executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                  int completed = migratedBlobs.incrementAndGet();
                   try {
-                    mongoDbJsonBlobManager.deleteBlob(id.toString());
-                  } catch (BlobNotFoundException e) {
-                    //this shouldn't happen...
+                    log.trace("Migrating blob {}", id);
+                    fileSystemJsonBlobManager.createBlob(json, id.toString());
+                    log.trace("Completed migrating blob {}", id);
+                  } catch (MongoException e) {
+                    log.warn("Error while migrating blob", e);
+                  } catch (Exception e) {
+                    log.warn("Caught exception while migrating blob", e);
+                  } finally {
+                    latch.countDown();
+                    if (completed % 25 == 0) {
+                      log.info("Migrated {} blobs... (~{} per second)", completed, completed / stopwatch.elapsed(TimeUnit.SECONDS));
+                    }
+                    try {
+                      mongoDbJsonBlobManager.deleteBlob(id.toString());
+                    } catch (BlobNotFoundException e) {
+                      //this shouldn't happen...
+                    }
                   }
                 }
-              }
-            });
+              });
+            } catch (NullPointerException e) {
+              mongoDbJsonBlobManager.deleteBlob(id.toString());
+            }
           }
       }
       latch.await();
