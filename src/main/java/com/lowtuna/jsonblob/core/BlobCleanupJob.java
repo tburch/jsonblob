5a52a25b2342231cc3929f5311d55a7e7c8c5a50
Tristan Burch <me@tristanburch.com>
2017-08-16 14:49:04 -0600

    add try/catch around everything

--- src/main/java/com/lowtuna/jsonblob/core/BlobCleanupJob.java
+++ src/main/java/com/lowtuna/jsonblob/core/BlobCleanupJob.java
@@ -48,42 +48,46 @@ public class BlobCleanupJob implements Runnable {
                 if (!dataDir.toFile().exists() || !dataDir.toFile().isDirectory()) {
                   return;
                 }
-                List<File> files = Arrays.asList(dataDir.toFile().listFiles()).parallelStream().filter(File::exists).collect(Collectors.toList());
-                Set<String> blobs = Sets
-                        .newHashSet(Lists.transform(files, f -> f.getName().split("\\.", 2)[0]))
-                        .parallelStream()
-                        .filter(f -> fileSystemJsonBlobManager.resolveTimestamp(f).isPresent()).collect(Collectors.toSet());
-                log.info("Identified {} blobs in {}", blobs.size(), dataDir);
-                Map<String, DateTime> lastAccessed = Maps.newHashMap(Maps.asMap(blobs, new Function<String, DateTime>() {
-                  @Nullable
-                  @Override
-                  public DateTime apply(@Nullable String input) {
-                    return fileSystemJsonBlobManager.resolveTimestamp(input).get();
-                  }
-                }));
-                log.info("Completed building map of {} last accessed timestamps in {}", lastAccessed.size(), dataDir);
-
-                File metadataFile = fileSystemJsonBlobManager.getMetaDataFile(dataDir.toFile());
                 try {
-                  BlobMetadataContainer metadataContainer = metadataFile.exists() ? om.readValue(fileSystemJsonBlobManager.readFile(metadataFile), BlobMetadataContainer.class) : new BlobMetadataContainer();
-                  log.info("Adding {} last accessed timestamp from metadata {}", metadataContainer.getLastAccessedByBlobId().size(), metadataFile.getAbsolutePath());
-                  lastAccessed.putAll(metadataContainer.getLastAccessedByBlobId());
-                  log.debug("Determining which blobs to remove from {}", dataDir);
-                  Map<String, DateTime> toRemove = Maps.filterEntries(lastAccessed, input -> input.getValue().plusMillis((int) blobAccessTtl.toMilliseconds()).isBefore(DateTime.now()));
-                  log.info("Identified {} blobs to remove in {}", toRemove.size(), dataDir);
-                  toRemove.keySet().parallelStream().forEach(blobId -> {
-                    if (deleteEnabled) {
-                      log.debug("Deleting blob with id {}", blobId);
-                      try {
-                        fileSystemJsonBlobManager.deleteBlob(blobId);
-                        blobsRemoved.incrementAndGet();
-                      } catch (BlobNotFoundException e) {
-                        log.debug("Couldn't delete blobId {} because it's already been deleted", blobId);
-                      }
+                  List<File> files = Arrays.asList(dataDir.toFile().listFiles()).parallelStream().filter(File::exists).collect(Collectors.toList());
+                  Set<String> blobs = Sets
+                          .newHashSet(Lists.transform(files, f -> f.getName().split("\\.", 2)[0]))
+                          .parallelStream()
+                          .filter(f -> fileSystemJsonBlobManager.resolveTimestamp(f).isPresent()).collect(Collectors.toSet());
+                  log.info("Identified {} blobs in {}", blobs.size(), dataDir);
+                  Map<String, DateTime> lastAccessed = Maps.newHashMap(Maps.asMap(blobs, new Function<String, DateTime>() {
+                    @Nullable
+                    @Override
+                    public DateTime apply(@Nullable String input) {
+                      return fileSystemJsonBlobManager.resolveTimestamp(input).get();
                     }
-                  });
-                } catch (IOException e) {
-                  log.warn("Couldn't load metadata file from {}", dataDir.toAbsolutePath(), e);
+                  }));
+                  log.debug("Completed building map of {} last accessed timestamps in {}", lastAccessed.size(), dataDir);
+
+                  File metadataFile = fileSystemJsonBlobManager.getMetaDataFile(dataDir.toFile());
+                  try {
+                    BlobMetadataContainer metadataContainer = metadataFile.exists() ? om.readValue(fileSystemJsonBlobManager.readFile(metadataFile), BlobMetadataContainer.class) : new BlobMetadataContainer();
+                    log.debug("Adding {} last accessed timestamp from metadata {}", metadataContainer.getLastAccessedByBlobId().size(), metadataFile.getAbsolutePath());
+                    lastAccessed.putAll(metadataContainer.getLastAccessedByBlobId());
+                    log.debug("Determining which blobs to remove from {}", dataDir);
+                    Map<String, DateTime> toRemove = Maps.filterEntries(lastAccessed, input -> input.getValue().plusMillis((int) blobAccessTtl.toMilliseconds()).isBefore(DateTime.now()));
+                    log.info("Identified {} blobs to remove in {}", toRemove.size(), dataDir);
+                    toRemove.keySet().parallelStream().forEach(blobId -> {
+                      if (deleteEnabled) {
+                        log.debug("Deleting blob with id {}", blobId);
+                        try {
+                          fileSystemJsonBlobManager.deleteBlob(blobId);
+                          blobsRemoved.incrementAndGet();
+                        } catch (BlobNotFoundException e) {
+                          log.debug("Couldn't delete blobId {} because it's already been deleted", blobId);
+                        }
+                      }
+                    });
+                  } catch (IOException e) {
+                    log.warn("Couldn't load metadata file from {}", dataDir.toAbsolutePath(), e);
+                  }
+                } catch (Exception e) {
+                  log.warn("Caught Exception while trying to remove un-accessed blobs in {}", dataDir, e);
                 }
               });
       log.info("Completed cleanup of {} blobs in {}ms", blobsRemoved.get(), stopwatch.elapsed(TimeUnit.MILLISECONDS));
