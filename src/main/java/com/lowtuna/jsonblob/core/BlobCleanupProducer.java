7301e6795c7524c2169052927d871092850a595f
Tristan Burch <me@tristanburch.com>
2017-08-20 13:48:03 -0600

    Use a directory Stream

--- src/main/java/com/lowtuna/jsonblob/core/BlobCleanupProducer.java
+++ src/main/java/com/lowtuna/jsonblob/core/BlobCleanupProducer.java
@@ -9,6 +9,7 @@ import org.apache.commons.io.DirectoryWalker;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.Collection;
@@ -34,39 +35,46 @@ public class BlobCleanupProducer extends DirectoryWalker<Void> implements Runnab
 
   @Override
   protected boolean handleDirectory(File directory, int depth, Collection<Void> results) throws IOException {
-    File[] files = directory.listFiles();
+    if (isDataDir(directory.getAbsolutePath())) {
+      String[] dateParts = directory.getAbsolutePath().replace(dataDirectoryPath.toFile().getAbsolutePath(), "").split("/", 4);
+      LocalDate localDate = LocalDate.of(Integer.parseInt(dateParts[1]), Integer.parseInt(dateParts[2]), Integer.parseInt(dateParts[3]));
+      boolean process = localDate.isBefore(LocalDate.now().minusDays(blobAccessTtl.toDays()));
+      if (process) {
+        log.info("Processing {} blobs for un-accessed blobs", directory.getAbsolutePath());
+        Files.newDirectoryStream(directory.toPath())
+                .forEach(path -> {
+                  File file = path.toFile();
+                  if (file.getName().startsWith(FileSystemJsonBlobManager.BLOB_METADATA_FILE_NAME)) {
+                    return;
+                  }
+                  try {
+                    filesToProcess.put(file);
+                  } catch (InterruptedException e) {
+                    log.warn("Interrupted while trying to add file to be processed at {}", file.getAbsolutePath(), e);
+                  }
+                });
+      }
 
-    if (files != null && files.length == 0) {
-      if (directory.delete()) log.info("{} has no files, so it's being deleted", directory.getAbsolutePath());
+      File[] files = directory.listFiles();
+      if (files != null && files.length == 1) {
+        if (files[0].getName().startsWith(FileSystemJsonBlobManager.BLOB_METADATA_FILE_NAME)) {
+          if (directory.delete()) {
+            log.info("{} has only a metadata file, so it's being deleted", directory.getAbsolutePath());
+          }
+        }
+      } else if (files.length == 0) {
+        log.info("{} has no files, so it's being deleted", directory.getAbsolutePath());
+      }
       return false;
-    }
-
-    if (files.length == 1) {
-      if (files[0].getName().startsWith(FileSystemJsonBlobManager.BLOB_METADATA_FILE_NAME)) {
-        if (directory.delete()) log.info("{} has only a metadata file, so it's being deleted", directory.getAbsolutePath());
+    } else {
+      File[] files = directory.listFiles();
+      if (files != null && files.length == 0) {
+        if (directory.delete()) log.info("{} has no files, so it's being deleted", directory.getAbsolutePath());
         return false;
       }
     }
 
-    boolean process = true;
-    if (isDataDir(directory.getAbsolutePath())) {
-      String[] dateParts = directory.getAbsolutePath().replace(dataDirectoryPath.toFile().getAbsolutePath(), "").split("/", 4);
-      LocalDate localDate = LocalDate.of(Integer.parseInt(dateParts[1]), Integer.parseInt(dateParts[2]), Integer.parseInt(dateParts[3]));
-      process = localDate.isBefore(LocalDate.now().minusDays(blobAccessTtl.toDays()));
-      if (process) {
-        log.info("Processing {} blobs for un-accessed blobs", directory.getAbsolutePath());
-        for (File file: files) {
-          try {
-            filesToProcess.put(file);
-          } catch (InterruptedException e) {
-            log.warn("Interrupted while trying to add file to be processed at {}", file.getAbsolutePath(), e);
-          }
-        }
-        process = false;
-      }
-    }
-
-    return process;
+    return true;
   }
 
   private boolean isDataDir(String path) {
